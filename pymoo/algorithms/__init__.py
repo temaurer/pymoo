70217c75ba452eaf0cf0a1c143f549677df25c50
temaurer <47586920+temaurer@users.noreply.github.com>
2019-07-08 10:41:41 +0800

    updated __init__ for main driver function

--- pymoo/algorithms/__init__.py
+++ pymoo/algorithms/__init__.py
@@ -1,26 +1,27 @@
 import numpy as np
 from pymoo.optimize import minimize
-from pymoo.algorithms.nsga3 import nsga3
+from pymoo.algorithms.unsga3 import unsga3
+from.pymoo.algorithms.ga import ga
 from pymoo.util import plotting
 from pymop.factory import get_problem
 
-#This is to test various provided functions
-
 # load a test or define your own problem
-problem = get_problem("zdt1")
+problem = get_problem("g01")
 
 # get the optimal solution of the problem for the purpose of comparison
-pf = problem.pareto_front()
+#pf = problem.pareto_front()
 
 # create the algorithm object
-method = nsga3(ref_dirs=np.array([[3., 4.],[2.,5],[4.,5],[2.,5]]), pop_size=100, elimate_duplicates=True)
+#method = unsga3(ref_dirs=np.array([[3.]]), pop_size=100, elimate_duplicates=True)
+
+method = ga(pop_size=100, elimate_duplicates=True)
 
 # execute the optimization
 res = minimize(problem,
                   method,
                   termination=('n_gen', 200),
-                  pf=pf,
+                  #pf=pf,
                   disp=True)
 
 # plot the results as a scatter plot
-plotting.plot(pf, res.F, labels=["Pareto-Front", "F"])
+#plotting.plot(pf, res.F, labels=["Pareto-Front", "F"])
